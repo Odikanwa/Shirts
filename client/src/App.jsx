6c47070e041125fafb50003702ec056de2fd8ed9
odikanwa <odikanwamichael@gmail.com>
2023-04-30 14:48:09 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import Home from './pages/Home';
+import Canvas from './canvas';
+import Customiser from './pages/Customiser';
+
+function App() {
+
+  return (
+   <main className='app transition-all ease-in'>
+    <Home/>
+    <Canvas/>
+    <Customiser/>
+   </main>
+  )
+}
+
+export default App
